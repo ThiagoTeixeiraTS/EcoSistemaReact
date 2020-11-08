d366d77124d0b5fe42003477a2be1b073b55f5cc
Thiago Teixeira <thiagoemecal@gmail.com>
2020-11-08 17:43:54 -0300

    Ecosistema React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Learn React</div>;
+class App extends React.Component {
+  state = {
+    nome: "Thiago",
+  };
+
+  modificarNome = (event) => {
+    let nome = event.target.value;
+    this.setState({
+      nome: nome,
+    });
+  };
+
+  criarComboBox = () => {
+    const opcoes = ["Fulano", "Ciclano"];
+    const comboBoxOpcoes = opcoes.map((opcao) => <option>{opcao}</option>);
+
+    return <select>{comboBoxOpcoes}</select>;
+  };
+
+  componentDidMount() {
+    console.log("Executou o DidMount ");
+  }
+
+  render() {
+    console.log("Executou o render");
+    const MetodoCriarComboBox = () => this.criarComboBox();
+    return (
+      <>
+        <input
+          type="text"
+          value={this.state.nome}
+          onChange={this.modificarNome}
+        />
+        <h1>Olá meu nome é {this.props.nome}</h1>
+
+        <MetodoCriarComboBox />
+      </>
+    );
+  }
 }
 
 export default App;
